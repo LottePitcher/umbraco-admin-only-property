9e9840f3f9803a2b354deeba60c557e424b76bf7
Lotte Pitcher <github@lottepitcher.co.uk>
2022-10-02 12:52:25 +0100

    add manifest filter

--- src/AdminOnlyProperty/AdminOnlyPropertyComposer.cs
+++ src/AdminOnlyProperty/AdminOnlyPropertyComposer.cs
@@ -1,5 +1,6 @@
 ﻿using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Manifest;
 using Umbraco.Cms.Core.Notifications;
 
 namespace Umbraco.Community.AdminOnlyProperty
@@ -8,7 +9,23 @@ namespace Umbraco.Community.AdminOnlyProperty
     {
         public void Compose(IUmbracoBuilder builder)
         {
+            builder.ManifestFilters().Append<AdminOnlyPropertyManifestFilter>();
             builder.AddNotificationHandler<SendingContentNotification, AdminOnlyPropertySendingContentNotificationHandler>();
+        }
+    }
+
+    internal class AdminOnlyPropertyManifestFilter : IManifestFilter
+    {
+        public void Filter(List<PackageManifest> manifests)
+        {
+            var assembly = typeof(AdminOnlyPropertyManifestFilter).Assembly;
+
+            manifests.Add(new PackageManifest
+            {
+                PackageName = AdminOnlyPropertyDataEditor.DataEditorName,
+                Version = assembly.GetName().Version.ToString(3),
+                AllowPackageTelemetry = true
+            });
         }
     }
 }
