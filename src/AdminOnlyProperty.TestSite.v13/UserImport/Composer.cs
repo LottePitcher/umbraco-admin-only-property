f6658269febbbb72f80ee9ba14996462c9316e0f
Lotte Pitcher <github@lottepitcher.co.uk>
2023-12-21 22:01:31 +0000

    add test site for v13.0.3

---
+++ src/AdminOnlyProperty.TestSite.v13/UserImport/Composer.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.Notifications;
+
+namespace AdminOnlyProperty.TestSite.v13.UserImport;
+
+public class Composer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.AddNotificationHandler<UmbracoApplicationStartedNotification, CreateEditorUser>();
+    }
+}
---
+++ src/AdminOnlyProperty.TestSite.v13/UserImport/CreateEditorUser.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Models.Membership;
+using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Core.Services;
+
+namespace AdminOnlyProperty.TestSite.v13.UserImport;
+
+public class CreateEditorUser : INotificationHandler<UmbracoApplicationStartedNotification>
+{
+    private readonly IUserService _userService;
+    private const string EditorEmail = "editor@editor.com";
+    private const string EditorPassword = "password1234567890";
+
+    public CreateEditorUser(IUserService userService) => _userService = userService;
+
+    public void Handle(UmbracoApplicationStartedNotification notification)
+    {
+        IUser? editorExists = _userService.GetByEmail(EditorEmail);
+
+        if (editorExists is not null)
+        {
+            return;
+        }
+
+        // Creating the user as Umbraco does in their tests:
+        // https://github.com/umbraco/Umbraco-CMS/blob/8e609af90168a7c5e088b30193266733269457ec/tests/Umbraco.Tests.Integration/Umbraco.Infrastructure/Services/UserServiceTests.cs#L728
+        HMACSHA1 hash = new()
+        {
+            Key = Encoding.Unicode.GetBytes(EditorPassword)
+        };
+
+        var encodedPassword = Convert.ToBase64String(hash.ComputeHash(Encoding.Unicode.GetBytes(EditorPassword)));
+        GlobalSettings globalSettings = new();
+
+        User membershipUser = new(
+            globalSettings,
+            "Editor",
+            EditorEmail,
+            EditorEmail,
+            encodedPassword);
+
+        if(_userService.GetUserGroupByAlias(Umbraco.Cms.Core.Constants.Security.EditorGroupAlias)
+           is not UserGroup editorUserGroup)
+        {
+            return;
+        }
+
+        membershipUser.AddGroup(editorUserGroup);
+
+        _userService.Save(membershipUser);
+    }
+}
